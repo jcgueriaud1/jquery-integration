89f037413dd53dc15b16e5535921c58068369225
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-03-01 19:12:20 +0200

    Initial commit

---
+++ src/main/java/com/example/MainView.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@JavaScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        H1 title = new H1("Test Page");
+        add(title);
+        getElement().executeJs("if (typeof jQuery !== 'undefined') { \n" +
+                "               alert(\"jQuery is loaded globally\");\n" +
+                "             } else {\n" +
+                "               alert(\"jQuery is not loaded globally\");\n" +
+                "             };");
+    }
+}
