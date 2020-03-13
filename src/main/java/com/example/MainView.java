20c5f246d3b5585d702182b5205e7183db2a22ca
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-03-13 16:59:41 +0200

    Add import

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.H1;
@@ -29,5 +30,4 @@ public class MainView extends VerticalLayout {
                 "             };");
     }
 
-}
 }
